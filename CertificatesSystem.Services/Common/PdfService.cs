255888821611ee75e438b302979bf1388ff7debf
Jaime Navarrete <jaimenava123@outlook.com>
2022-06-05 20:53:38 -0600

    Crea servicio que realiza la funcionalidad de extraer la información de los estudiantes desde los PDFs

---
+++ CertificatesSystem.Services/Common/PdfService.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using Spire.Pdf;
+
+namespace CertificatesSystem.Services.Common;
+
+public static class PdfService
+{
+    public static void ExtractPdfNies()
+    {
+        var pdfDocument = new PdfDocument();
+        pdfDocument.LoadFromFile(@"C:\Documentos\matriculasSecciones.pdf");
+
+        var text = new StringBuilder();
+        foreach (PdfPageBase page in pdfDocument.Pages)
+        {
+            text.Append(page.ExtractText());
+        }
+        pdfDocument.Close();
+
+        var textLines = text.Replace("Evaluation Warning : The document was created with Spire.PDF for .NET.", "").Replace(",", " ,").Replace("\r\n","\n").ToString().Split("\n");
+        var studentsInfo = new List<string>();
+
+        for (var index = 0; index < textLines.Length; index++)
+        {
+            textLines[index] = Regex.Replace(textLines[index], @"\s+", " ");
+            textLines[index] = textLines[index].Trim();
+
+            if (string.IsNullOrEmpty(textLines[index])) continue;
+            
+            var words = textLines[index].Split(" ");
+
+            if (!int.TryParse(words[0], out _)) continue;
+
+            var studentDataPosition = words.Length - 2;
+            
+            var info = words[1..studentDataPosition];
+            studentsInfo.Add(string.Join(" ", info));
+        }
+
+        var fileName = @"C:\Documentos\TextInPdf.txt";
+
+        File.WriteAllText(fileName, string.Join("\n", studentsInfo));
+    }
+}
